4c16c08683835e3fa38f38976314d8601c7343ce
emadleo <hamidleo1984@gmai.com>
2026-05-24 22:33:19 +0300

    update supabase

--- lib/core/config/supabase_config.dart
+++ lib/core/config/supabase_config.dart
@@ -1,5 +1,4 @@
 class SupabaseConfig {
-  // Get these from: Supabase Dashboard → Settings → API
-  static const url     = 'https://YOUR_PROJECT_ID.supabase.co';
-  static const anonKey = 'YOUR_ANON_KEY_HERE';
+  static const url     = 'https://colahcvziorjkqckqdlt.supabase.co';
+  static const anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNvbGFoY3Z6aW9yamtxY2txZGx0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzNDUzMTMsImV4cCI6MjA5MzkyMTMxM30.hGS1fUGaGThzr9x80SAsKh6Xfidmv2ChTFtVVkZzKY4';
 }
